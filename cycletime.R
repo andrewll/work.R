6c084214145e402dc4ffbe2464066b6621a92abd
andrewll <andrew@llorente.org>
2015-10-06 18:34:27 -0700

    Signed-off-by: andrewll <andrew@llorente.org>

--- cycletime.R
+++ cycletime.R
@@ -15,15 +15,33 @@ cycletime <- function(file1){
   library(dplyr)
   library(lubridate)
   ##mydf <- read.csv(file1, stringsAsFactors = FALSE)
-  mydf<-read.csv("C:/Users/andrewll/Documents/R/MCIOdata/All/DelCap-Jul1-Sep28-alleg-networkandservers.csv", stringsAsFactors = FALSE)
+  mydf<-read.csv("C:/Users/andrewll/Documents/R/MCIOdata/All/DelCap-Jul1-Sep30-alleg-networkandservers.csv", stringsAsFactors = FALSE)
   dat <- tbl_df(mydf)
   
   ##select desired columns from the dataframe and add some new ones for month and year delivered
-  dat2 <- subset(dat, select=c("EG","DeliveryNumber","ProjectTitle","RTEGOTDF","RTEGActualDeliveryDate", "ProjectCategory", "OrderCOnfirmtoDock_Actual", "DTR1", "OrderCOnfirmtoRTEG_Actual2","ProjectCreationDate","woadPOCreation","woadDock2"))
+  dat2 <- subset(dat, select=c("EG",
+                               "DeliveryNumber",
+                               "ProjectTitle",
+                               "RTEGOTDF",
+                               "RTEGActualDeliveryDate", 
+                               "ProjectCategory", 
+                               "OrderCOnfirmtoDock_Actual", 
+                               "DTR1", 
+                               "OrderCOnfirmtoRTEG_Actual2",
+                               "ProjectCreationDate",
+                               "woadPOCreation",
+                               "woadDock2"))
   
   ## add calculated variables
-  dat3 <- mutate(dat2, Month_Delivered = format(mdy(RTEGActualDeliveryDate),"%b"), Year_Delivered = format(mdy(RTEGActualDeliveryDate),"%Y"), PIDCount = 1, pidcreate = mdy(ProjectCreationDate), pocreate = mdy(woadPOCreation), dockdate = mdy(woadDock2))
-  dat4 <- mutate(dat3, pidcreate_to_pocreate = as.numeric((pocreate - pidcreate) / 86400), pocreate_to_dock = as.numeric((dockdate - pocreate)/86400))
+  dat3 <- mutate(dat2, Month_Delivered = format(mdy(RTEGActualDeliveryDate),"%b"), 
+                 Year_Delivered = format(mdy(RTEGActualDeliveryDate),"%Y"), 
+                 PIDCount = 1, 
+                 pidcreate = as.Date(ProjectCreationDate, format = "%m/%d/%Y"), 
+                 pocreate = as.Date(woadPOCreation, format = "%m/%d/%Y"), 
+                 dockdate = as.Date(woadDock2, format = "%m/%d/%Y"))
+  
+  dat4 <- mutate(dat3, pidcreate_to_pocreate = pocreate - pidcreate, 
+                 pocreate_to_dock = dockdate - pocreate)
  
   ##summarize and create report table
   dat5<- dat4 %>% 
@@ -37,8 +55,13 @@ cycletime <- function(file1){
               Dock_to_RTEG_Avg = mean(DTR1, na.rm=TRUE),  
               Dock_to_RTEG_95th = quantile(DTR1, .95, na.rm=TRUE)) %>%
     arrange(EG, ProjectCategory, Year_Delivered, Month_Delivered)
-    
-  write.csv(dat5,file="ouput_cycletime.csv")
+  
+  ##print table with individual PID details
+  write.csv(dat4,file="C:/Users/andrewll/Documents/R/MCIOdata/All/ouput_cycletime_pid_details.csv")
+  
+  ##print table with summarized details by EG
+  write.csv(dat5,file="C:/Users/andrewll/Documents/R/MCIOdata/All/ouput_cycletime_pid_summaries.csv")
+  
   View(dat5)
   
   ## create PRD charts
