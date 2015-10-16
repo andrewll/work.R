e47707718feb9f66adfc57d5815f8dc581447a47
andrewll <andrew@llorente.org>
2015-10-16 16:04:27 -0700

    Signed-off-by: andrewll <andrew@llorente.org>

--- simulaterteg.R
+++ simulaterteg.R
@@ -8,7 +8,14 @@ simulaterteg<-function(){
   ##
   #########################
   
+  ## read data
+  dat1 <- read.csv("C:/Users/andrewll/OneDrive - Microsoft/WindowsPowerShell/Data/in/DeliveryPerformanceWithMilestone.csv", stringsAsFactors = TRUE)
   
+  ##convert dates into correct format
+  dat1$RTEGActualDeliveryDate <- as.Date(dat1$RTEGActualDeliveryDate, format = "%m/%d/%Y")
+  
+  ## select desired variables
+  dat2<- subset(dat1, select = c("DeliveryNumber","DeploymentClass","EG","ProjectCategory","RTEGActualDeliveryDate"))
   
   
   
